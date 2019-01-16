0f96f3f07e482618df2ea350081d1244289bc4b4
Sm2927 <44033974+Sm2927@users.noreply.github.com>
2019-01-16 21:35:40 +0530

    using regex

--- multimap.cpp
+++ multimap.cpp
@@ -43,4 +43,45 @@ int main(){
     }
     return 0;
 }
+//using regex
 
+int main(){
+
+    int N;
+
+    cin >> N;
+
+    regex e(".+@gmail\\.com$");
+
+    vector<string> db;
+
+    for(int a0 = 0; a0 < N; a0++){
+
+        string firstName;
+
+        string emailID;
+
+        cin >> firstName >> emailID;
+
+
+        if(regex_match(emailID,e)){
+
+            db.push_back(firstName);
+
+        }
+
+    }
+
+    sort(db.begin(),db.end()); 
+
+    for(int i = 0;i < db.size();i++) 
+
+        { 
+
+        cout << db[i] << endl; 
+
+    }
+
+    return 0;
+
+}
