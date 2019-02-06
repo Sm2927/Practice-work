dc1c08609df0c2802a82aa26b2e9cbccefbf8195
Sm2927 <44033974+Sm2927@users.noreply.github.com>
2019-02-06 12:59:03 +0530

    hint for constant memory

--- pair/insertingintoset.cpp
+++ pair/insertingintoset.cpp
@@ -26,3 +26,16 @@ vector<int> Solution::repeatedNumber(const vector<int> &A) {
     return r;
     
 }
+//hint for solving without using extra memory
+/*
+m(Actual) = Sum(1...N) + A - B
+
+    Sum(Actual) - Sum(1...N) = A - B. 
+
+    Sum(Actual Squares) = Sum(1^2 ... N^2) + A^2 - B^2
+
+    Sum(Actual Squares) - Sum(1^2 ... N^2) = (A - B)(A + B) 
+
+    = (Sum(Actual) - Sum(1...N)) ( A + B). 
+We can use the above 2 equations to get the value of A and B.
+*/
