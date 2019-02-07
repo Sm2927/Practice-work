94b91ee8d3452f468c7fc5325916e527c8b2ae6e
Sm2927 <44033974+Sm2927@users.noreply.github.com>
2019-02-07 17:45:52 +0530

    finding number of digits

---
+++ tricks/numberofdidgits.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long int n;
+    cin>>n;
+    int d;
+    d = floor(log10(n)+1); //to find the number of digits in the binary representation of the number write: d = floor(log2(n)+1);
+    cout<<d;
+   
+
+}
