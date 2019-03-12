f64dbdf162c6dca33ace7293758dd1227032b31b
Sm2927 <44033974+Sm2927@users.noreply.github.com>
2019-03-12 15:33:10 +0530

    simple modulo

--- hackerrank challenges/kangaroo.cpp
+++ hackerrank challenges/kangaroo.cpp
@@ -14,6 +14,19 @@ string kangaroo(int x1, int v1, int x2, int v2) {
 
     }
     return "NO";
+   
+   
+   
+   
+   //or
+   /*
+    if(v1 > v2){
+        if((x2 - x1) %(v1 - v2) == 0){
+            return "YES";
+        }
 
+    }
+    return "NO";
+*/
 }
 
