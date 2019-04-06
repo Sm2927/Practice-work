beb350a95cb522e8db7ed21f22e0e35de786f4cc
Sm2927 <44033974+Sm2927@users.noreply.github.com>
2019-04-06 06:56:28 +0530

    Update roundRobin.cpp

--- os/scheduling/roundRobin.cpp
+++ os/scheduling/roundRobin.cpp
@@ -13,29 +13,46 @@ int main(){
         int q;
         cin>>q;
         int t=0;
-        
-        bool st = true;
-        while(1){
-            st = true;
+        int sum = 0;
+        int st[n]={0};
+       bool check = true;
+      
+        while(true){
+            sum = 0;
             for(int i=0;i<n;i++){
                 if(rem[i] > 0){
-                    st = false;
                     if(rem[i] > q){
                         t += q;
                         rem[i] -= q;
+                       // if(i == n-1){cout<<rem[i]<<endl;}
                     }
                     else{
                         t += rem[i];
-                        wt[i] = t - bt[i];
                         rem[i] = 0;
+                        st[i] = 1;
+                        wt[i] = t - bt[i];
+                        //if(i == n-1){cout<<rem[i]<<endl;}
                     }
                 }
             }
-            if(st == true){
+             
+           
+            for(int i=0;i<n;i++){
+                sum += st[i];
+               // cout<<rem[i]<<endl;
+            }
+            //cout<<endl<<endl;
+           // cout<<sum<<endl;
+            if(sum == n){
+               // check =false;
                 break;
             }
         }
-        for(int i=0;i<n;i++){
+        
+        
+        
+        
+               for(int i=0;i<n;i++){
             cout<<wt[i]<<endl;
         }
         
