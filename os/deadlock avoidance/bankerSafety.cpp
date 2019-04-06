c913d0e15e9f737b21871251f633f9da8ac098c4
Sm2927 <44033974+Sm2927@users.noreply.github.com>
2019-04-06 07:34:45 +0530

    Update bankerSafety.cpp

--- os/deadlock avoidance/bankerSafety.cpp
+++ os/deadlock avoidance/bankerSafety.cpp
@@ -5,7 +5,7 @@ int main(){
     n=5;
     r=3;
     int av[r]={3, 3, 2};
-    int work[r] = {3, 3, 2}; 
+   // int work[r] = {3, 3, 2}; 
    // bool f[n]={false};
     int max[n][r]={{7, 5, 3}, 
                      {3, 2, 2}, 
@@ -24,36 +24,37 @@ int main(){
         }
         
     }
+   
+
+    int work[r]={3,3,2};
     
     int f[n]={0};
     int safe[n];
     int cnt=0;
-    bool flag;
-    while(cnt < n){
-        flag = false;
-        for(int i=0;i<n;i++){
-            if(f[i] == 0){
-                int j;
-                for(j=0;j<r;j++){
-                    if(need[i][j] > work[j])
-                        {break;}
-                }
-                if(j == r){
+   // bool flag;
+   while(cnt<n){
+       for(int i=0;i<n;i++){
+           if(f[i] == 0){
+               int j;
+             for(j=0;j<n;j++){
+                if(need[i][j] > work[j]){break;}
+                if(j==r){
                     for(int k=0;k<r;k++){
                         work[k] += all[i][k];
                     }
-                    safe[cnt++] = i;
                     f[i] = 1;
-                    flag = true;
+                    safe[cnt++] = i;
                 }
-            }
-        }
-        if(flag == false){
-            cout<<"not safe"<<endl;
-            return 0;
-        }
+             }  
+           }
+           
+       }
+   }
+   for(int i=0;i<n;i++){
+       if(f[i] == 0){
+        cout<<"not safe"<<endl;}
     }
-    
+   
     for(int i=0;i<n;i++){
         cout<<safe[i]<<endl;
     }
