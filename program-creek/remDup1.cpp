50288e643cc2c54fc8a7d722eea7b9e2eb5406b7
Sovna Mohanty <sovnamohanty19@gmail.com>
2019-06-24 19:42:28 +0530

    2 pointer approach to maintain constant space and linear time

--- program-creek/remDup1.cpp
+++ program-creek/remDup1.cpp
@@ -3,10 +3,14 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        int s = nums.size();
+        if(s<2){
+            return s;
+        }
         int i,j;
         i=1;
         j=0;
-        int s = nums.size();
+        
         while(i<s){
             if(nums[i] != nums[j])  {
                 // 2 pointer approach to maintain constant space and linear time
