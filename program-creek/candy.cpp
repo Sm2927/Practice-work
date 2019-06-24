f31bd8ffc8566b08d70c17aa3b2c36dead5848a8
Sovna Mohanty <sovnamohanty19@gmail.com>
2019-06-24 20:48:38 +0530

    Create candy.cpp

---
+++ program-creek/candy.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/candy/
+
+class Solution {
+public:
+    int candy(vector<int>& a) {
+        int s = a.size();
+        int c = s;
+        int p,n;
+        int i = 0;
+        while(i<s){
+            p=n=0;
+            if(i != 0 && a[i] > a[i-1]){
+                p = 1;
+            }
+            if(i != s-1 && a[i] > a[i+1]){
+                n = 1;
+            }
+            if(p+n > 0){c++;}
+            i++;
+        }
+        return c;
+    }
+};
