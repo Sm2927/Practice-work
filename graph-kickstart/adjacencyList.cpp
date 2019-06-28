00386666dcc78eeb8e06e3947b5ad7d8a090b22b
Sovna Mohanty <sovnamohanty19@gmail.com>
2019-06-28 18:23:27 +0530

    graph representation using vector

---
+++ graph-kickstart/adjacencyList.cpp
@@ -0,0 +1,46 @@
+// A simple representation of graph using STL 
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// A utility function to add an edge in an 
+// undirected graph. 
+void addEdge(vector<vector<int>> &adj, int u, int v) 
+{ 
+    //adj.push_back(std::vector<int>());
+	adj[u].push_back(v); 
+	adj[v].push_back(u); 
+  	
+} 
+
+// A utility function to print the adjacency list 
+// representation of graph 
+void printGraph(vector<vector<int>> &adj, int V) 
+{ 
+	for (int v = 0; v < V; ++v) 
+	{ 
+		cout << "\n Adjacency list of vertex "
+			<< v << "\n head "; 
+		for (auto x : adj[v]) 
+		cout << "-> " << x; 
+		printf("\n"); 
+	} 
+} 
+
+// Driver code 
+int main() 
+{ 
+	int V = 5; 
+	vector<vector<int>> adj;
+    adj.resize(V);
+  	cout<<adj.size()<<endl;
+  	cout<<adj[0].size()<<endl;
+	addEdge(adj, 0, 1); 
+	addEdge(adj, 0, 4); 
+	addEdge(adj, 1, 2); 
+	addEdge(adj, 1, 3); 
+	addEdge(adj, 1, 4); 
+	addEdge(adj, 2, 3); 
+	addEdge(adj, 3, 4); 
+	printGraph(adj, V); 
+	return 0; 
+} 
