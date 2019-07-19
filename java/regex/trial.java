fc2b71e69717cc07a11b816ba8d104d81bfd37c8
Sovna Mohanty <sovnamohanty19@gmail.com>
2019-07-20 00:36:58 +0530

    Create trial.java

---
+++ java/regex/trial.java
@@ -0,0 +1,20 @@
+//https://www.programcreek.com/2013/10/top-10-questions-for-java-regular-expression/
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.regex.*;
+import java.text.*;
+
+class GFG {
+	public static void main (String[] args) {
+	    int cnt = 0;
+		Pattern p = Pattern.compile("(?x) (?:  a  (?= a* (\\1?+ b))  )+ \\1");
+		Matcher m = p.matcher("aaaabbbbaabbaaaabbbb");
+		while(m.find()){
+		    System.out.println(m.start()+"   "+m.end());
+		    cnt++;
+		}
+		System.out.println(cnt);
+	}
+}
