a86c11cfcca1bbd69de0b0e38371df2b74a1d905
Sovna Mohanty <sovnamohanty19@gmail.com>
2019-07-21 19:18:10 +0530

    Update bitset.java

--- quickcheck/bitset.java
+++ quickcheck/bitset.java
@@ -1,32 +1,49 @@
-//https://www.geeksforgeeks.org/bitset-class-java-set-1/
 
-// Java program illustrating Bitset Class constructors. 
+//https://www.geeksforgeeks.org/bitset-class-methods-java-examples-set-2/
+// Java program explaining BitSet class methods 
+// xor(), length(), clear() methods 
 import java.util.*; 
-public class GFG 
+public class NewClass 
 { 
 	public static void main(String[] args) 
 	{ 
-		// Constructors of BitSet class 
 		BitSet bs1 = new BitSet(); 
-		BitSet bs2 = new BitSet(6); 
+		BitSet bs2 = new BitSet(); 
 
-		/* set is BitSet class method 
-		expalined in next articles */
-		bs1.set(0); 
+		// assign values to bs1 using set() 
+		bs1.set(7); 
 		bs1.set(1); 
 		bs1.set(2); 
 		bs1.set(4); 
+		bs1.set(3); 
+		bs1.set(6); 
 
 		// assign values to bs2 
 		bs2.set(4); 
 		bs2.set(6); 
-		bs2.set(5); 
-		bs2.set(1); 
-		bs2.set(2); 
 		bs2.set(3); 
+		bs2.set(9); 
+		bs2.set(2); 
 
-		// Printing the 2 Bitsets 
+		// Printing the Bitsets 
 		System.out.println("bs1 : " + bs1); 
 		System.out.println("bs2 : " + bs2); 
+
+		// use of length() method 
+		System.out.println("use of length() : " + bs1.length()); 
+
+		// use of xor() to perform logical Xor operation 
+		bs1.xor(bs2); 
+		System.out.println("Use of xor() : " + bs1); 
+		bs2.xor(bs1); 
+		System.out.println("Use of xor() : " + bs2); 
+
+		// clear from index 2 to index 4 in bs1 
+		bs2.clear(1, 2); 
+		System.out.println("bs2 after clear method : " + bs2); 
+
+		// clear complete Bitset 
+		bs1.clear(); 
+		System.out.println("bs1 after clear method : " + bs1); 
 	} 
 } 
